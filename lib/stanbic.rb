aa5e82734fa8713e964ee5d1be0e0082bcd661cf
Ochieng Otieno <ochiengotieno71@gmail.com>
2022-11-24 19:32:08 +0300

    first commit

---
+++ lib/stanbic.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "stanbic/version"
+
+module Stanbic
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/stanbic/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Stanbic
+  VERSION = "0.1.0"
+end
